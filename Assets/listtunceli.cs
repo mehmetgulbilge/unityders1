79140f5710eaf9d8409befae1f09479bc04d1363
mehmetgulbilge <m.gulbilge@gmail.com>
2025-05-20 02:17:38 +0300

    Add list manipulation script to manage names and numbers
    Introduced `listtunceli` class with basic list operations in Unity. Includes adding, inserting, and iterating over integer and string lists, demonstrating fundamental collection handling.

---
+++ Assets/listtunceli.cs
@@ -0,0 +1,35 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class listtunceli : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        List<int> sayilar = new List<int>();
+        sayilar.Add(50);
+        sayilar.Add(40);
+        sayilar.Add(18);
+
+        List<string> isimler = new List<string>();
+
+        isimler.Add("Mehmet");
+        isimler.Add("Ahmet");
+        isimler.Add("Murat");
+
+        // Debug.Log(isimler[0]);
+        // Debug.Log(isimler[1]);
+        // Debug.Log(isimler[2]);
+        isimler.Insert(1, "Ebru");
+        foreach (var isim in isimler)
+        {
+            Debug.Log(isim);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
