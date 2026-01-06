c5ad78b9d98e931f1af5ada020182b2224293d23
mehmetgulbilge <m.gulbilge@gmail.com>
2026-01-06 03:48:19 +0300

    Expand `Ilkdosyam` script with new `cocuklar` and `babalar` fields, update `SampleScene` to reflect changes

--- Assets/Scripts/Ilkdosyam.cs
+++ Assets/Scripts/Ilkdosyam.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Ilkdosyam : MonoBehaviour
@@ -6,6 +7,9 @@ public class Ilkdosyam : MonoBehaviour
     public int Yas;
     public float Maas;
     public bool ArabaVarMi;
+
+    public string[] cocuklar;
+    public List<string> babalar;
     private string Memleket;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -16,6 +20,8 @@ public class Ilkdosyam : MonoBehaviour
         Debug.Log("Yaşım " + Yas + " ve maaşım " + Maas);
         if (Yas == 18) ArabaVarMi = true;
         else ArabaVarMi = false;
+
+        foreach (var cocuk in cocuklar) Debug.Log(cocuk);
     }
 
     // Update is called once per frame
