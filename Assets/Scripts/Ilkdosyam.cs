30801d282876e97b587ba58b4919b83416fe3a08
mehmetgulbilge <m.gulbilge@gmail.com>
2026-01-06 18:39:30 +0300

    Disable `CapsuleCollider` in `Ilkdosyam` script, adjust initialization logic, and update `SampleScene` to reflect changes

--- Assets/Scripts/Ilkdosyam.cs
+++ Assets/Scripts/Ilkdosyam.cs
@@ -10,22 +10,36 @@ public class Ilkdosyam : MonoBehaviour
 
     public string[] cocuklar;
     public List<string> babalar;
+
+    private CapsuleCollider Capsulum;
     private string Memleket;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     private void Start()
     {
-        Memleket = "Sivas";
-        Debug.Log("Merhaba, benim adım " + Ad);
-        Debug.Log("Yaşım " + Yas + " ve maaşım " + Maas);
-        if (Yas == 18) ArabaVarMi = true;
-        else ArabaVarMi = false;
+        Capsulum = GetComponent<CapsuleCollider>();
 
-        foreach (var cocuk in cocuklar) Debug.Log(cocuk);
+        Capsulum.enabled = false;
+        Capsulum.isTrigger = true;
+        Capsulum.height = 7;
+
+        var capsullerim = GetComponents<CapsuleCollider>();
+        foreach (var capsulum in capsullerim) capsulum.enabled = false;
     }
 
     // Update is called once per frame
     private void Update()
     {
     }
+
+    // private void Start()
+    // {
+    //     Memleket = "Sivas";
+    //     Debug.Log("Merhaba, benim adım " + Ad);
+    //     Debug.Log("Yaşım " + Yas + " ve maaşım " + Maas);
+    //     if (Yas == 18) ArabaVarMi = true;
+    //     else ArabaVarMi = false;
+    //
+    //     foreach (var cocuk in cocuklar) Debug.Log(cocuk);
+    // }
 }
