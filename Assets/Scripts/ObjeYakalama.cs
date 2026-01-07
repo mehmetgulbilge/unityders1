b5b074b003234c7fba89de26d6a88a9f9b9536cf
mehmetgulbilge <m.gulbilge@gmail.com>
2026-01-07 03:30:59 +0300

    Refactor `ObjeYakalama` script to include `disaridan` field, adjust tagging for GameObjects in `SampleScene`, and update object positioning

--- Assets/Scripts/ObjeYakalama.cs
+++ Assets/Scripts/ObjeYakalama.cs
@@ -2,14 +2,39 @@ using UnityEngine;
 
 public class ObjeYakalama : MonoBehaviour
 {
-    private GameObject[] nesneler;
+    public GameObject disaridan;
+    private GameObject[] _nesneler;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     private void Start()
     {
-        nesneler = GameObject.FindGameObjectsWithTag("Player");
+        if (gameObject.CompareTag("Player"))
+            Debug.Log("Player");
+        else
+            Debug.Log("Not Player");
+        // gameObject.SetActive(true);
+        // if (gameObject.activeSelf)
+        //     Debug.Log(true);
+        // else Debug.Log(false);
 
-        foreach (var nesne in nesneler) nesne.GetComponent<Ilkdosyam>().Yas = 34;
+        // Camera cam = (Camera)FindAnyObjectByType(typeof(Camera));
+        // if (cam != null)
+        //     Debug.Log("Camera found: " + cam.name);
+        // else
+        //     Debug.Log("Camera not found");
+
+        // Genel obje bulma
+        // Rigidbody capsulum = GetComponent(typeof(Rigidbody)) as Rigidbody;
+        // Rigidbody capsulum = GetComponent("Rigidbody") as Rigidbody;
+        // capsulum!.mass = 3f;
+        // _nesneler = GameObject.FindGameObjectsWithTag("Player");
+        //
+        // foreach (GameObject nesne in _nesneler)
+        // {
+        //     nesne.GetComponent<Ilkdosyam>().Yas = 34;
+        //     Debug.Log("Yas set to 34 for " + nesne.name);
+        // }
+
         // GameObject.FindWithTag("Player").GetComponent<Rigidbody>().mass = 2;
         // GameObject.Find("Ana").GetComponent<Rigidbody>().angularDamping = 0.5f;
         // Debug.Log("Angular damping set to 0.5 for Ana object");
