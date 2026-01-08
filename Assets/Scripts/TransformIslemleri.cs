59b32335777187125bb5076249eb1c9ace09a204
mehmetgulbilge <m.gulbilge@gmail.com>
2026-01-08 23:04:12 +0300

    Refactor `TransformIslemleri` script to clean up unused logic in `Start` and `Update` methods, add child name logging, and enhance debugging. Update `TransformIslem` scene by replacing GameObjects, adjusting layers, and modifying component properties.

--- Assets/Scripts/TransformIslemleri.cs
+++ Assets/Scripts/TransformIslemleri.cs
@@ -11,16 +11,28 @@ public class TransformIslemleri : MonoBehaviour
     private void Start()
     {
         // transform.position = new Vector3(transform.position.x, 3f, transform.position.z);
-        hedef.transform.Rotate(new Vector3(90f, 0f, 0f), Space.Self);
-        hedef2.transform.Rotate(new Vector3(90f, 0f, 0f), Space.World);
+        // hedef.transform.Rotate(new Vector3(90f, 0f, 0f), Space.Self);
+        // hedef2.transform.Rotate(new Vector3(90f, 0f, 0f), Space.World);
+
+
+        // GameObject aramasonuc = transform.Find("GameObject").gameObject;
+        // aramasonuc.name = "Bulundum";
+        //
+        // Debug.Log(transform.childCount);
+        // Debug.Log(transform.hierarchyCount);
+
+        int childCount = transform.childCount;
+
+        for (int i = 0; i < childCount; i++) Debug.Log(transform.GetChild(i).name);
     }
 
     // Update is called once per frame
     private void Update()
     {
+        // hedef2.transform.LookAt(hedef.position);
         // transform.Translate(transform.forward * (Time.deltaTime * 2f));
         // transform.Rotate(new Vector3(2f, 0f, 0f) * (Time.deltaTime * 2f));
-        hedef.transform.Rotate(new Vector3(2f, 4f, 1f) * (Time.deltaTime * 10f), Space.Self);
-        hedef2.transform.Rotate(new Vector3(2f, 4f, 1f) * (Time.deltaTime * 10f), Space.World);
+        // hedef.transform.Rotate(new Vector3(2f, 4f, 1f) * (Time.deltaTime * 10f), Space.Self);
+        // hedef2.transform.Rotate(new Vector3(2f, 4f, 1f) * (Time.deltaTime * 10f), Space.World);
     }
 }
