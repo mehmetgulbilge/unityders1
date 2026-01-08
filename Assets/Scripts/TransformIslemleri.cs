ad85fa87abd5d6dc9315f53a1a74ac9b74a52874
mehmetgulbilge <m.gulbilge@gmail.com>
2026-01-08 22:00:54 +0300

    Update `TransformIslemleri` script to simplify `Start` and `Update` methods by removing unused vector operations and adding `Rotate` logic for target Transforms. Update `TransformIslem` scene with two new GameObjects, including their components and properties adjustments.

--- Assets/Scripts/TransformIslemleri.cs
+++ Assets/Scripts/TransformIslemleri.cs
@@ -6,45 +6,21 @@ public class TransformIslemleri : MonoBehaviour
 
     public Transform hedef2;
 
-    private float BaslangicZamani;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     private void Start()
     {
-        BaslangicZamani = Time.time;
-
-        Vector3 birincipoz = new(4f, 2f, 3f);
-        Vector3 ikinciPoz = new(1f, 2f, 5f);
-
-        // print(Vector3.Max(birincipoz, ikinciPoz));
-        // print(Vector3.Min(birincipoz, ikinciPoz));
-        // print(Vector3.Scale(birincipoz, ikinciPoz));
-        print(birincipoz - ikinciPoz);
-        print(birincipoz + ikinciPoz);
-        print(birincipoz * 2f);
-        print(birincipoz / 2f);
+        // transform.position = new Vector3(transform.position.x, 3f, transform.position.z);
+        hedef.transform.Rotate(new Vector3(90f, 0f, 0f), Space.Self);
+        hedef2.transform.Rotate(new Vector3(90f, 0f, 0f), Space.World);
     }
 
     // Update is called once per frame
     private void Update()
     {
-        // transform.position = Vector3.Lerp(transform.position, hedef.position, (Time.time - BaslangicZamani) * 3f);
-
-        /*
-         * Distance
-         */
-        // float distance = Vector3.Distance(hedef.position, transform.position);
-        // Debug.Log(distance);
-
-        /*
-         * Angle
-         */
-        // Vector3 mesafe = hedef.position - transform.position;
-
-        // float angle = Vector3.Angle(mesafe, transform.forward);
-        //
-        // Debug.Log(angle);
-        //
-        // if (angle < 5f) Debug.Log("Angle is less than 5 degrees");
+        // transform.Translate(transform.forward * (Time.deltaTime * 2f));
+        // transform.Rotate(new Vector3(2f, 0f, 0f) * (Time.deltaTime * 2f));
+        hedef.transform.Rotate(new Vector3(2f, 4f, 1f) * (Time.deltaTime * 10f), Space.Self);
+        hedef2.transform.Rotate(new Vector3(2f, 4f, 1f) * (Time.deltaTime * 10f), Space.World);
     }
 }
