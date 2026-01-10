dd6621288ed867987504208b376cfc874bdc2777
mehmetgulbilge <m.gulbilge@gmail.com>
2026-01-11 02:59:35 +0300

    Add `HideFlagsOrnek` script to demonstrate Unity's `HideFlags` usage. Update `TransformIslem` scene with new script reference and adjusted transform values.

---
+++ Assets/Scripts/HideFlagsOrnek.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+
+[AddComponentMenu("Beni Ekle")]
+public class HideFlagsOrnnek : MonoBehaviour
+{
+    [Header("Karakter Bilgileri")] public string saglik;
+
+    public string para;
+    public string level;
+
+    [Header("Düşman Bilgileri")] public string dusman_saglik;
+
+    public string dusman_para;
+    public string dusman_level;
+
+    // [Multiline(3)] public string ad;
+    //
+    // [Range(1, 10)] public int sayi;
+    //
+    // [HideInInspector] public string soyad;
+    //
+    // [Tooltip("Aktif mi?")] public bool aktif = true;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private void Start()
+    {
+        // gameObject.hideFlags = HideFlags.DontSave;
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+    }
+
+    [ContextMenu("Menum")]
+    private void Menum()
+    {
+        print("Ayarlardan geliyorum");
+    }
+}
