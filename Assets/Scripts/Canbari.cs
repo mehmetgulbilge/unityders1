59bc6af0f169392aed747d3cea4f23a9c0011779
mehmetgulbilge <m.gulbilge@gmail.com>
2026-03-02 02:37:02 +0300

    Add `Canbari` script for managing image UI fill amount using keyboard input and function calls. Update scene and import relevant assets (`beyaz.png`) and metadata.

---
+++ Assets/Scripts/Canbari.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.UI;
+
+public class Canbari : MonoBehaviour
+{
+    Image image;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        image = (Image)GameObject.Find("Canvas/Image").GetComponent<Image>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Keyboard.current.spaceKey.wasPressedThisFrame)
+        {
+            image.fillAmount -= 0.1f;
+        }
+    }
+
+    public void candusur()
+    {
+        image.fillAmount -= 0.1f;
+    }
+}
