44f780f746c121653492ded5885e9e8d1eb6450e
mehmetgulbilge <m.gulbilge@gmail.com>
2026-03-27 13:37:44 +0300

    Add `_Recovery` scene with initial lighting, NavMesh, and GameObject configurations. Update `karakter.controller` with new transitions and conditions. Enhance `KarakterScriptim` for keyboard-based animation controls. Remove unused AI packages from project.

--- Assets/Scripts/KarakterScriptim.cs
+++ Assets/Scripts/KarakterScriptim.cs
@@ -3,8 +3,9 @@ using UnityEngine.InputSystem;
 
 public class KarakterScriptim : MonoBehaviour
 {
-    
+
     public Animator animator;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -17,6 +18,46 @@ public class KarakterScriptim : MonoBehaviour
         if (Keyboard.current.wKey.wasPressedThisFrame)
         {
             animator.SetBool("yuru_p", true);
+        }
+        else if (Keyboard.current.wKey.wasReleasedThisFrame)
+        {
+            animator.SetBool("yuru_p", false);
+        }
+        
+        if (Keyboard.current.shiftKey.wasPressedThisFrame)
+        {
+            animator.SetBool("kos_p", true);
+            
+            if (Keyboard.current.ctrlKey.wasPressedThisFrame)
+            {
+                animator.SetBool("depar_p", true);
+            }
+        }
+        
+        
+        if (Keyboard.current.ctrlKey.wasReleasedThisFrame)
+        {
+            animator.SetBool("depar_p", false);
+        }
+        
+        else if (Keyboard.current.shiftKey.wasReleasedThisFrame)
+        {
+            animator.SetBool("kos_p", false);
+        }
+        
+        if (Keyboard.current.altKey.wasReleasedThisFrame)
+        {
+            animator.SetBool("kos_p", false);
+        }
+        
+        
+        if (Keyboard.current.spaceKey.wasPressedThisFrame)
+        {
+            animator.SetBool("zipla_p", true);
+        }
+        else if (Keyboard.current.spaceKey.wasReleasedThisFrame)
+        {
+            animator.SetBool("zipla_p", false);
         }
     }
 }
